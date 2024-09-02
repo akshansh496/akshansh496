ead41e8858221207238f7f93e423aec7b26cd552
akshansh496 <guptaakshansh1@gmail.com>
2024-09-02 16:00:32 +0530

    Add files via upload

---
+++ practise_dictionary.py
@@ -0,0 +1,8 @@
+dict={}
+a=int(input("Enter marks in mathematics"))
+dict.update({"Maths":"a"})
+b=int(input("Enter marks in physics"))
+dict.update({"Physics":"b"})
+c=int(input("Enter marks in chemistry"))
+dict.update({"Chemistry":"c"})
+print(dict)
