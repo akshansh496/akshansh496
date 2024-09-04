7fbb21026843521612d393564636006066886247
akshansh496 <guptaakshansh1@gmail.com>
2024-09-04 15:56:48 +0530

    Add files via upload

---
+++ function_even_odd.py
@@ -0,0 +1,11 @@
+x=int(input("input a number"))
+def even_odd(z):
+    if z%2==0:
+        return True
+    else:
+        return False
+if(even_odd(x)==True):
+    print("Even")
+else:
+    print("Odd")
+ 
