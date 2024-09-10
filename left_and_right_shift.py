5f7be7e2ea9496ecdb6c32090844319f35d648d8
akshansh496 <guptaakshansh1@gmail.com>
2024-09-10 12:45:41 +0530

    Add files via upload

---
+++ left_and_right_shift.py
@@ -0,0 +1,5 @@
+x=int(input("Enter a number"))
+left_shift=x<<2
+right_shift=x>>2
+print(left_shift)
+print(right_shift)
---
+++ power_of_two.py
@@ -0,0 +1,5 @@
+x=int(input("Enter a number"))
+if(x&(x-1)==0):
+    print("power of two")
+else:
+    print("Not power of two")
